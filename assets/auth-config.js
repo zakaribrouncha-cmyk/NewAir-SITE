93d6d35e6ac268f5cd3e1ae2e32ebf9c1a4ddeff
zakaribrouncha-cmyk <zakaribrouncha@gmail.com>
2026-07-07 23:21:56 +0200

    Ajout configuration Discord

---
+++ assets/auth-config.js
@@ -0,0 +1,8 @@
+// Configuration NewAir.
+// Pour activer une vraie connexion Discord, crée une application sur le portail Discord Developer.
+// Ensuite ajoute ton URL de retour, par exemple : https://TON-DOMAINE/login.html
+// Remplace la valeur ci-dessous par le Client ID de ton application.
+window.NEWAIR_AUTH = {
+  DISCORD_CLIENT_ID: "TON_CLIENT_ID_DISCORD",
+  REDIRECT_URI: ""
+};
